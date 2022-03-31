dd2e9ee86abf5563694613c6499a67732f4ba9fe
Ibrahim <17607802+cukcan@users.noreply.github.com>
2022-03-31 20:31:26 +0300

    Update dogAge.js

--- dogAge.js
+++ dogAge.js
@@ -4,5 +4,5 @@ earlyYears *= 10.5;
 let laterYears = myAge - 2;
 laterYears *= 4;
 let myAgeInDogYears = earlyYears + laterYears;
-let myName = "cukcan";
+let myName = "Ibrahim";
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
